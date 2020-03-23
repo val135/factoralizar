455ad78f9e57a0c2e1b7fb9f0a9e56b71bf86f9b
Dubenis <dubenis85201@gmail.com>
2020-03-23 14:20:45 -0500

    add js code

---
+++ punto3.js
@@ -0,0 +1,31 @@
+'use strict';
+const boton=document.querySelector(`.submit-button`);
+
+boton.addEventListener(`click`,(e)=>{
+    let numInput=document.querySelector(`.pick-number`);
+    let save=numInput.value;
+    event.preventDefault(e);
+    numInput=parseFloat(numInput.value);
+    if(numInput>=0 && numInput<=1){
+        numInputb=1;
+    }
+    else if(numInput>1){
+        for (var numInputb = 1; numInput > 0; numInput--) {
+            numInputb *= numInput;
+      }
+    } 
+    document.querySelector('h1').innerHTML=`El Factorial de ${save} es: ${numInputb}`;
+})
+
+/*function factorialize(numInput) {
+    if(numInput>=0 && numInput<=1){
+        numInputb=1;
+    }
+    else if(numInput>1){
+        for (var numInputb = 1; numInput > 0; numInput--) {
+            numInputb *= numInput;
+      }
+    } 
+      return numInputb;
+    }
+     console.log(factorialize(-2));*/
